1b4f2a4be0d3b355f8578982bed26e3b04ecbdf1
johnson <shijianyue47@gmail.com>
2018-09-18 08:43:27 +0800

    chore(lint): add lint configuration

--- bin.js
+++ bin.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 
-const dzh = require('./dist/dzh.common.js');
-const program = require('commander');
+const program = require('commander')
+const dzh = require('./dist/dzh.common.js')
 
 program
-    .version('1.0.0')
-    .option('-n, --number <value>', 'number of greetings, 1 by default', parseInt)
-    .parse(process.argv);
+  .version('1.0.0')
+  .option('-n, --number <value>', 'number of greetings, 1 by default', parseInt)
+  .parse(process.argv)
 
 const times = Number(program.number) || 1
 
 for (let i = 0; i < times; i++) {
-    setTimeout(() => {
-        console.log(i + 1, dzh.say())
-    }, 500);
+  setTimeout(() => {
+    // eslint-disable-next-line
+    console.log(i + 1, dzh.say())
+  }, 500)
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import greetings from './data.json'
 
+export const random = () => Math.floor(Math.random() * greetings.length)
+
 export function say() {
   return greetings[random()]
 }
-
-export const random = () =>
-  Math.floor(Math.random() * greetings.length)
 
 export default say
